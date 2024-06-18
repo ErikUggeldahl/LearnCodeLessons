fe873bfa3b73175e1eb381394155582c27653e75
Erik Uggeldahl <erikuggeldahl@gmail.com>
2024-06-17 19:22:20 -0700

    Init with IDX template

---
+++ idx-template.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }: {
+
+  # Shell script that produces the final environment
+  bootstrap = ''
+    # Copy the folder containing the `idx-template` files to the final
+    # project folder for the new workspace. ${./.} inserts the directory
+    # of the checked-out Git folder containing this template.
+    cp -rf ${./.} "$out"
+
+    # Set some permissions
+    chmod -R +w "$out"
+
+    # Remove the template files themselves and any connection to the template's
+    # Git repository
+    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+  '';
+
+  packages = [
+    pkgs.nodejs_21
+    pkgs.rustfmt
+    pkgs.rustup
+    pkgs.stdenv.cc
+  ];
+
+  env = {
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+  };
+
+  idx = {
+    extensions = [
+      "rust-lang.rust-analyzer"
+      "tamasfe.even-better-toml"
+      "serayuzgur.crates"
+      "vadimcn.vscode-lldb"
+    ];
+  };
+}
